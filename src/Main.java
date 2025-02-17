07c56410d9fa1371e537aa79d74cca3c800096a7
Amir Dev <altymyshevamir@gmail.com>
2025-02-17 13:25:29 +0600

    git push

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        MyClass myClass = new MyClass("Amir", "Altymyshev", 18, new String[]{"Java", "English", "Soft"}, "Hamburger");
+
+        System.out.println("Me -> ");
+        System.out.println("{ Name : " + myClass.name);
+        System.out.println("Surname : " + myClass.surName);
+        System.out.println("Age : " + myClass.age);
+        System.out.println("Lesson : " + Arrays.toString(myClass.lesson));
+        System.out.println("My favorite foot : " + myClass.foot + " } ");
+
+        System.out.println("\n-------------------------------\n");
+
+        MyClass myClass1 = new MyClass("Altymyshev", "Amir", 18, new String[]{"Java", "JS", "C++", "C#"});
+
+        System.out.println("Me -> ");
+        System.out.println("{ Surname : " + myClass1.surName);
+        System.out.println("Name : " + myClass1.name);
+        System.out.println("Age : " + myClass1.age);
+        System.out.println("Lesson : " + Arrays.toString(myClass1.lesson) + " } ");
     }
 }
--- src/MyClass.java
+++ src/MyClass.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class MyClass {
+
+    String name;
+    String surName;
+    int age;
+    String[] lesson;
+    String foot;
+
+    public MyClass(String name, String surName, int age, String[] lesson, String foot) {
+        this.name = name;
+        this.surName = surName;
+        this.age = age;
+        this.lesson = lesson;
+        this.foot = foot;
+    }
+
+    public MyClass(String surName, String name, int age, String[] lesson) {
+        this.surName = surName;
+        this.name = name;
+        this.age = age;
+        this.lesson = lesson;
+    }
 }
